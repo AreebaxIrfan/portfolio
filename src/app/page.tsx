4f55e98e0667097310cfa8f0471f6424721a1df4
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-18 10:57:04 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import Footer from "@/components/landingPage/footer";
 export default function Portfolio() {
   return (
     <div className="min-h-screen bg-white">
+      
       {/* <AreebaAssistant /> */}
       <main className="container mx-auto space-y-16">
         <HeroSection />
